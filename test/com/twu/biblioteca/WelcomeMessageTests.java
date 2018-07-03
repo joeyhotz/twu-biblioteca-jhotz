7425506a9e96d2e70e92e00cb0f4b930f0a89bed
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-07-03 16:22:55 +1000

    renames tests

--- test/com/twu/biblioteca/WelcomeMessageTests.java
+++ test/com/twu/biblioteca/WelcomeMessageTests.java
@@ -8,18 +8,15 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
+public class WelcomeMessageTests {
 
     @Test
     public void testWelcomeMessage() {
         String expectedWelcomeMessage = "Hello! Welcome to Biblioteca.";
         String actualWelcomeMessage = null;
-        PrintStream originalOut = System.out;
-
         try {
             ByteArrayOutputStream os = new ByteArrayOutputStream();
             PrintStream capture = new PrintStream(os);
-            // From this point on, everything printed to System.out will get captured
             System.setOut(capture);
             BibliotecaApp.main(new String[]{});
             capture.flush();
