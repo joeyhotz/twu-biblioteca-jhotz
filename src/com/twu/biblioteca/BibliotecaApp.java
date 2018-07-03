a5531398c93142c251220a06197ff68fb5a9c21c
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-07-03 16:23:33 +1000

    fixes code to pass test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,6 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.print("Hello, world!");
+        System.out.print("Hello! Welcome to Biblioteca.");
     }
 }
