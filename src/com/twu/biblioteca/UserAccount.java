28c04964faa78932ab4f3afc45e45a330ae4c835
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-07-05 11:13:48 +1000

    program now hashses passwords

--- src/com/twu/biblioteca/UserAccount.java
+++ src/com/twu/biblioteca/UserAccount.java
@@ -6,11 +6,11 @@ public class UserAccount {
 
     public UserAccount(String libraryNumber, String password) {
         this.libraryNumber = libraryNumber;
-        this.password = password;
+        this.password = MD5PasswordHashing.hashPassword(password);
     }
 
     public static UserAccount loginValid(String libraryNumber, String password) {
-        if (libraryNumber.equals("123") && password.equals("test")) {
+        if (libraryNumber.equals("123") && password.equals(MD5PasswordHashing.hashPassword("test"))) {
             return new UserAccount("123", "test");
         }
         return null;
--- test/com/twu/biblioteca/UserAccountTests.java
+++ test/com/twu/biblioteca/UserAccountTests.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.*;
 public class UserAccountTests {
     @Test
     public void testLoginWorks() {
-        assertTrue(UserAccount.loginValid("123", "test") != null);
+        assertTrue(UserAccount.loginValid("123", MD5PasswordHashing.hashPassword("test")) != null);
     }
 
     @Test
     public void testLoginDetectsInvalidAccount() {
-        assertTrue(UserAccount.loginValid("invalid", "invalid") == null);
+        assertTrue(UserAccount.loginValid("invalid", MD5PasswordHashing.hashPassword("invalid")) == null);
     }
 }
